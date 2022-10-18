f667c7fd803e73dc8b2783e9d46419ca6eeaf54e
unknown <deltahackall@gmail.com>
2022-10-18 22:07:26 +0900

    make ascii convert method

--- cam.py
+++ cam.py
@@ -1,40 +1,42 @@
 from hashlib import algorithms_available
+from lib2to3.pytree import convert
 import cv2 as cv
 from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 
 density = 'Ñ@#W$9876543210?!abc;:+=-,._ '
+asciiresolution = (50,50)
+fontsize = 10
 
 def setResolution(cam, width, height):
     cam.set(3, width)
     cam.set(4, height)
 
-def converttoASCII(frame, size=(30, 30)):
-    originalsize = (len(frame), len(frame[0]))
+def converttoASCII(frame, size=asciiresolution):
     frame = cv.resize(frame, size)
     cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # print(len(frame), len(frame[0]))
-    fnt = ImageFont.truetype("arial.ttf", 1)
-    img = Image.new('L', size, color = 'black')
+    fnt = ImageFont.truetype("arial.ttf", fontsize)
+    img = Image.new('L', (size[0]*fontsize, size[1]*fontsize), color = 'black')
     draw = ImageDraw.Draw(img)
 
     for i in range(size[0]):
         for j in range(size[1]):
             light = density[int(sum(frame[i][j])/(256*3) * len(density))]
-            draw.text((i, j), light, fill="white", font=fnt, spacing=0, align="center")
+            draw.text((j*fontsize, i*fontsize), light, fill="white", font=fnt, spacing=0, align="center")
 
     img = np.array(img)
     return img
 
-def openWebcam(size=(300, 400)):
-    cap = cv.VideoCapture("test.mp4")
+def openWebcam(size=(300, 400), source=0):
+    cap = cv.VideoCapture(source)
     w, h = size
     setResolution(cap, w, h)
 
     while(True):
         ret, frame = cap.read()
         if(ret):
-            
+            frame = converttoASCII(frame)
             cv.imshow('camera', frame)
             if cv.waitKey(1) & 0xFF == 27: # esc 키를 누르면 닫음
                 # print(frame)
@@ -45,5 +47,4 @@ def openWebcam(size=(300, 400)):
     
 
 if __name__=="__main__":
-    size = (200, 100)
-    openWebcam(size)
+    openWebcam()
